315c0de8b44b9c19f2fc700369568afb591704dd
tristanmatthew <tristanmatthew@gmail.com>
2021-02-09 16:08:38 -0500

    information now loading to page

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 const url = 'https://itunes.apple.com/search?term='
 const searchSubmit = document.querySelector('#search-submit')
 const searchResults = document.querySelector('#results')
+// let searchList 
 
 
 // event listeners
@@ -13,17 +14,49 @@ searchSubmit.addEventListener('submit', (event) => {
     let searchTerm = document.querySelector('#search-bar').value
     console.log('search bar working', searchTerm)
 
-    function search() {
+    // fetch request to API for iTunes search
+
+    function searchList() {
         fetch (`${url}+${searchTerm}`)
             .then (res => res.json())
             .then (data => {
                 console.log(data)
-                let artist = data.artist
-                console.log (artist)
+               for (let song of data.results) {
+                    renderResults(song)
+               }
             })
     }
-    search()
+    searchList()
 })
+function renderResults(song) {
+    let resultDiv = document.createElement('div')
+        resultDiv.className = "results"
+    
+    let collectionImg = document.createElement('img')
+        collectionImg.className = "collection-img"
+        collectionImg.src= song.artworkUrl100
+    
+    let artistName = document.createElement('p')
+        artistName.className = "artist-name"
+        artistName.innerHTML = song.artistName
+    
+    let songTitle = document.createElement('p')
+        songTitle.className = "song-title"
+        songTitle.innerHTML = song.trackName
 
-// fetch request to API for iTunes search
+    
+    let collectionTitle = document.createElement('p')
+        collectionTitle.className = "collection-title"
+        collectionTitle.innerHTML = song.collectionName
 
+    // resultDiv.appendChild(collectionImg, artistName, songTitle, collectionTitle)
+    
+    resultDiv.appendChild(collectionImg)
+    resultDiv.appendChild(artistName)
+    resultDiv.appendChild(songTitle)
+    resultDiv.appendChild(collectionTitle)
+
+    searchResults.appendChild(resultDiv)
+}
+
+// searchList()
