20cb2a63956a8bbcf8b066d366eedc4ca531dff4
tristanmatthew <tristanmatthew@gmail.com>
2021-02-09 20:58:02 -0500

    search clears on new search, songs show in console for audio preview

--- main.js
+++ main.js
@@ -3,38 +3,90 @@
 // declare variables
 
 const url = 'https://itunes.apple.com/search?term='
-const searchSubmit = document.querySelector('#search-submit')
-const searchResults = document.querySelector('#results')
-// let searchList 
+let searchSubmit = document.querySelector('#search-submit')
+let searchResults = document.querySelector('#results')
+let audioPlayer = document.querySelector('#audio-player-container')
 
 
 // event listeners
 searchSubmit.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let searchTerm = document.querySelector('#search-bar').value
-    console.log('search bar working', searchTerm)
-
+    event.preventDefault();
+    clearResults();
+    searchList();
+    
     // fetch request to API for iTunes search
-
+    
     function searchList() {
+        let searchTerm = document.querySelector('#search-bar').value
         fetch (`${url}+${searchTerm}`)
-            .then (res => res.json())
-            .then (data => {
-                console.log(data)
-               for (let song of data.results) {
-                    renderResults(song)
-               }
-            })
+        .then (res => res.json())
+        .then (data => {
+            console.log(data)
+            for (let song of data.results) {
+                renderResults(song)
+            }
+        })
     }
-    searchList()
+    // searchList();
 })
+
+
+    
+document.addEventListener('click', e=> {
+    
+        if (e.target.className === ".resultDiv" ) {
+            let audioPlayerContainer = e.target.parentElement.querySelector("#audio-player-container")
+            let audioPlayer = audioPlayerContainer.querySelector('.track')
+    
+            if (audioPlayerContainer.classList.contains('hidden')) {
+                audioPlayerContainer.classList.remove('hidden')
+                audioPlayer.play();
+            }
+            else {
+                audioPlayerContainer.classList.add('hidden')
+                audioPlayer.pause()
+            }
+        }
+    })
+
+
+// a failed experiment for now. 
+// function renderAudio(song) {
+//     let audioDiv= document.createElement('div')
+//         audioDiv.className = "audio"
+//         audioDiv.classList.add('hidden')
+        
+//     let audio = document.createElement('audio')
+//         audio.className = 'track-preview'
+//         audio.src = song.previewUrl
+//         audio.controls = true
+
+
+//     audioDiv.appendChild(audio)
+//     audioPlayer.appendChild(audioDiv)
+// }
+// renderAudio()
+
+
 function renderResults(song) {
     let resultDiv = document.createElement('div')
         resultDiv.className = "results"
+
+    let audioDiv= document.createElement('div')
+        audioDiv.className = "audio"
+        audioDiv.classList.add('hidden')
+        
+    let audio = document.createElement('audio')
+        audio.className = 'track-preview'
+        audio.src = song.previewUrl
+        audio.controls = true
+
+        audioDiv.appendChild(audio)
+        audioPlayer.appendChild(audioDiv)
     
     let collectionImg = document.createElement('img')
         collectionImg.className = "collection-img"
-        collectionImg.src= song.artworkUrl100
+        collectionImg.src = song.artworkUrl100
     
     let artistName = document.createElement('p')
         artistName.className = "artist-name"
@@ -48,15 +100,26 @@ function renderResults(song) {
     let collectionTitle = document.createElement('p')
         collectionTitle.className = "collection-title"
         collectionTitle.innerHTML = song.collectionName
-
-    // resultDiv.appendChild(collectionImg, artistName, songTitle, collectionTitle)
     
+    let releaseDate = document.createElement('p')
+        releaseDate.className = "release-date"
+        let releaseYear = new Date(song.releaseDate)
+        const options = { year: 'numeric'}
+        releaseDate.innerHTML = "Year: "+releaseYear.toLocaleDateString('de-DE', options);
+
     resultDiv.appendChild(collectionImg)
     resultDiv.appendChild(artistName)
     resultDiv.appendChild(songTitle)
     resultDiv.appendChild(collectionTitle)
+    resultDiv.appendChild(releaseDate)
 
     searchResults.appendChild(resultDiv)
 }
 
+function clearResults() {
+    let songs = document.querySelectorAll('.results')
+    for (let song of songs) {
+        song.remove();
+    }
+}
 // searchList()
