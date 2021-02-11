28312d9d03b5c3a5b32d2fa09a2190730b69befa
tristanmatthew <tristanmatthew@gmail.com>
2021-02-11 12:50:19 -0500

    she's almost ready

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let searchSubmit = document.querySelector('#search-submit')
 let searchResults = document.querySelector('#results')
 let audioPlayer = document.querySelector('#audio-player-container')
 let container = document.querySelector('.container')
-let playerContainer = document.querySelector('.player-container')
 let imgContainer = document.querySelector('.img-container')
+let playerInfo = document.querySelector('.player-info')
 
-// event listeners
+// event listeners- on submisison run the following functions. 
 searchSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
     clearResults();
@@ -31,35 +31,55 @@ searchSubmit.addEventListener('submit', (event) => {
     }
 })
 
+// event listener on the search button - if it hits the preview button, then it's go time
 
-
-    
 searchResults.addEventListener('click', (event) => {
     event.preventDefault();
     console.log('clicked', event)
-    console.log(event.target.dataset.songUrl)
+    console.log(event.target.dataset)
     
     if (event.target.className === 'preview-button') {
     audioPlayer.src = event.target.dataset.songUrl
     audioPlayer.autoplay = 'true'
     audioPlayer.volume = .5;
-    displaySong()    
+    displaySong(event.target.dataset)    
     }
     else {
         // do nothing
     }
 })
 
-function displaySong() {
+// function to display the dynamic song information in the player area
+function displaySong(song) {
+
+    imgContainer.innerHTML = ""
+    playerInfo.innerHTML = ""
 
     let playerImg = document.createElement('img')
-        playerImg.className = "player-div"
+        playerImg.className = "player-img-div"
         playerImg.src = song.artworkUrl100
+        console.log(song.artworkUrl100)
         console.log('playerImg', playerImg)
 
+    let songTitle = document.createElement('p')
+        songTitle.className = "player-song-title"
+        songTitle.innerHTML = song.trackName
+        console.log(song.trackName)
+        console.log('songTitle', songTitle)
+
+    let artistName = document.createElement('p')
+        artistName.className = "player-artist-name"
+        artistName.innerHTML = song.artistName
+        console.log(song.artistName)
+        console.log('artistName', artistName)
+    
     imgContainer.appendChild(playerImg)
+    playerInfo.appendChild(songTitle)
+    playerInfo.appendChild(artistName)
+
 }
 
+// function to display the results of the search
 function renderResults(song) {
     let resultDiv = document.createElement('div')
         resultDiv.className = "results-div"
@@ -109,6 +129,9 @@ function renderResults(song) {
         previewButton.className = "preview-button"
         previewButton.innerText ='Play Preview'
         previewButton.dataset.songUrl = song.previewUrl
+        previewButton.dataset.artworkUrl100 = song.artworkUrl100
+        previewButton.dataset.trackName = song.trackName
+        previewButton.dataset.artistName = song.artistName
 
     trackImgDiv.appendChild(collectionImg)
     trackInfoDiv.appendChild(artistName)
@@ -122,6 +145,7 @@ function renderResults(song) {
     searchResults.appendChild(resultDiv)
 }
 
+// function for clearing the results on new search
 function clearResults() {
     let songs = document.querySelectorAll('.results-div')
     for (let song of songs) {
